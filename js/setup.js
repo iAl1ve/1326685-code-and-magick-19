d66f2dd67c63a91bf0e228d08eb98fc78b338d6d
Виталий Москвин <moskvin.vitaly@gmail.com>
2020-01-28 16:51:41 +0300

    5. Учебный проект: нас орда

---
+++ js/setup.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+// Генерируем случайное целое число в диапазоне, включая минимальное и максимальное, для генерации свойств персонажа
+var getRandomInRange = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Генерируем случайное имя персонажа, если mode = 0(false), то меняем местами имя и фамилию
+var getRandomName = function (mode) {
+  var fullName = mode ? WIZARD_NAMES[getRandomInRange(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomInRange(0, WIZARD_SURNAMES.length - 1)] : WIZARD_SURNAMES[getRandomInRange(0, WIZARD_SURNAMES.length - 1)] + ' ' + WIZARD_NAMES[getRandomInRange(0, WIZARD_NAMES.length - 1)];
+  return fullName;
+};
+
+var wizards = [
+  {
+    name: getRandomName(getRandomInRange(0, 1)),
+    coatColor: WIZARD_COAT_COLOR[getRandomInRange(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandomInRange(0, WIZARD_EYES_COLOR.length - 1)]
+  },
+  {
+    name: getRandomName(getRandomInRange(0, 1)),
+    coatColor: WIZARD_COAT_COLOR[getRandomInRange(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandomInRange(0, WIZARD_EYES_COLOR.length - 1)]
+  },
+  {
+    name: getRandomName(getRandomInRange(0, 1)),
+    coatColor: WIZARD_COAT_COLOR[getRandomInRange(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandomInRange(0, WIZARD_EYES_COLOR.length - 1)]
+  },
+  {
+    name: getRandomName(getRandomInRange(0, 1)),
+    coatColor: WIZARD_COAT_COLOR[getRandomInRange(0, WIZARD_COAT_COLOR.length - 1)],
+    eyesColor: WIZARD_EYES_COLOR[getRandomInRange(0, WIZARD_EYES_COLOR.length - 1)]
+  }
+];
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var addListWizard = function (ListElement) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+
+  ListElement.appendChild(fragment);
+};
+
+var buttonClose = document.querySelector('.setup-close');
+// Добавим событие на закрытие окна с настройкой персонажа
+buttonClose.addEventListener('click', function () {
+  userDialog.classList.add('hidden');
+});
+// Закрытие формы по нажатию ESC
+document.addEventListener('keydown', function (evt) {
+  // Проверяем, что код клавиши равен 27 - ESC
+  if (evt.keyCode === 27) {
+    userDialog.classList.add('hidden');
+  }
+});
+
+addListWizard(similarListElement);
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
